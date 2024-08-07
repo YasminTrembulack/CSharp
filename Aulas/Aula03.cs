51d616fa3c1f02d80d0eeeacf6c0d1a6ea647c85
YasminTrembulack <yasmin.agostinho012@gmail.com>
2024-08-07 14:33:07 -0300

    aula 04

--- Aulas/Aula03.cs
+++ Aulas/Aula03.cs
@@ -7,7 +7,7 @@
 // print("Hello world!");
 
 
-
+// Console.WriteLine("-----------------------");
 // int minhaFuncao(int a, int b)
 // {
 //     return a + b;
@@ -17,13 +17,13 @@
 // print(result_soma.ToString());
 
 
-
+// Console.WriteLine("-----------------------");
 // Func<int, int, int> sub = (a, b) => a - b;
 // var result_sub = sub(5, 2);
 // print(result_sub.ToString());
 
 
-
+// Console.WriteLine("-----------------------");
 // var soma = (int[] array) =>
 // {
 //     int total = 0;
@@ -35,6 +35,7 @@
 // print(result_arr_soma.ToString());
 
 
+// Console.WriteLine("-----------------------");
 
 // var chamaNVezes = (Action Func, int n) =>
 // {
@@ -52,6 +53,7 @@
 // // tem como parametro um int e me retorna uma função com retorno int
 
 
+// Console.WriteLine("-----------------------");
 // // da um coleção de dados e o tamanho de cada pagina
 // Func<int[], int, Func<int, int[]>> paginator = (dados, tamanho) =>
 // {
@@ -69,3 +71,78 @@
 // var pagina02 = paginas(2);
 // foreach (var item in pagina02)
 //     Console.WriteLine(item);
+
+
+// Console.WriteLine("-----------------------");
+// Func<Func<int>> closure = () =>
+// {
+//     int count = 0;
+//     return () => {
+//         count++;
+//         return count;
+//     };
+//     // return () => ++count;
+// };
+// var contador = closure();
+// Console.WriteLine(contador());
+// Console.WriteLine(contador());
+// Console.WriteLine(contador());
+// Console.WriteLine(contador());
+
+// //Ta errado
+// // Func<Object, (Func<Object> data, Action<dynamic> setData)> useSatate = (object data) =>
+// // {
+// //     var value = data;
+// //     return () => value, (object newValue) => value = newValue;
+
+// // }
+
+// Console.WriteLine("-----------------------");
+
+
+// List<int> listaMinha = [1, 2, 3, 4, 5, 6, 7, 8];
+// var pares = listaMinha.Where((element) => element % 2 == 0);
+// foreach (var item in pares)
+//     Console.WriteLine(item);
+
+
+
+// Console.WriteLine("-----------------------");
+// var pares_maior = listaMinha
+//             .Where((element) => element % 2 == 0)
+//             .Where((x) => x > 2);
+// foreach (var item in pares_maior)
+//     Console.WriteLine(item);
+
+
+
+// Console.WriteLine("-----------------------");
+// var select = listaMinha.Select((x) => x * 2);
+// foreach (var item in select)
+//     Console.WriteLine(item);
+
+// public static class Enumerable
+// {
+//     public static IEnumerable<T> Where<T>(
+//         this IEnumerable<T> collection,
+//         Func<T, bool> filter
+//     )
+//     {
+//         foreach (var item in collection)
+//             if (filter(item))
+//                 yield return item;
+
+//     }
+//     public static IEnumerable<R> Select<T, R>(
+//         this IEnumerable<T> collection,
+//         Func<T, R> mapper
+//     )
+//     {   
+//         ArgumentNullException.ThrowIfNull(collection, nameof(collection));
+//         ArgumentNullException.ThrowIfNull(mapper, nameof(mapper));
+
+//         var it = collection.GetEnumerator();
+//         while(it.MoveNext())
+//             yield return mapper(it.Current);
+//     }
+// }
--- Program.cs
+++ Program.cs
@@ -98,8 +98,6 @@ Console.WriteLine(contador());
 // }
 
 Console.WriteLine("-----------------------");
-
-
 List<int> listaMinha = [1, 2, 3, 4, 5, 6, 7, 8];
 var pares = listaMinha.Where((element) => element % 2 == 0);
 foreach (var item in pares)
@@ -120,6 +118,72 @@ Console.WriteLine("-----------------------");
 var select = listaMinha.Select((x) => x * 2);
 foreach (var item in select)
     Console.WriteLine(item);
+
+
+
+Console.WriteLine("-----------------------");
+List<string> list_str = ["1", "2", "3", "4", "5", "6", "7", "8"];
+var list_int = list_str.Select(int.Parse);
+foreach (var item in list_int)
+    Console.WriteLine(item);
+
+
+
+Console.WriteLine("----------- Aula 04 ------------");
+var take_while = listaMinha.TakeWhile((x) => x < 5);
+foreach (var item in take_while)
+    Console.WriteLine(item);
+
+
+
+Console.WriteLine("-----------------------");
+var skip_while = listaMinha.SkipWhile((x) => x < 5);
+foreach (var item in skip_while)
+    Console.WriteLine(item);
+
+
+
+Console.WriteLine("-----------------------");
+Pessoa yasmin = new("yasmin", 18);
+Pessoa eduardo = new("eduardo", 19);
+Pessoa julia = new("julia", 16);
+
+Pessoa[] pessoas = [yasmin, eduardo, julia];
+
+var maior_idade = pessoas.MaxBy((p) => p.Idade);
+Console.WriteLine(maior_idade);
+
+
+
+Console.WriteLine("-----------------------");
+var any_false = listaMinha.Any((x) => x % 13 == 0);
+Console.WriteLine(any_false);
+var any_true = listaMinha.Any((x) => x % 7 == 0);
+Console.WriteLine(any_true);
+
+
+
+Console.WriteLine("-----------------------");
+var aggregate = listaMinha.Aggregate((x, y) => x + y, 10);
+Console.WriteLine(aggregate);
+
+public class Pessoa{
+
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+
+    public Pessoa(string nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+    public override string ToString()
+    {
+        return "Nome: " + Nome + " Idade: " + Idade;
+    }
+}
+
+
 
 public static class Enumerable
 {
@@ -145,9 +209,74 @@ public static class Enumerable
         while(it.MoveNext())
             yield return mapper(it.Current);
     }
+    public static IEnumerable<T> TakeWhile<T>(
+        this IEnumerable<T> collection,
+        Func<T, bool> predicate
+    )
+    {   
+        ArgumentNullException.ThrowIfNull(collection, nameof(collection));
+        
+        foreach (var item in collection)
+        {
+            if(!predicate(item))
+                break;
+            yield return item;
+        }
+    }
+    public static IEnumerable<T> SkipWhile<T>(
+        this IEnumerable<T> collection,
+        Func<T, bool> predicate
+    )
+    {   
+        ArgumentNullException.ThrowIfNull(collection, nameof(collection));
+        
+        var canReturn = false;
+        foreach (var item in collection){
+            if(!predicate(item))
+                canReturn = true;
+            if(canReturn)
+                yield return item;
+        }
+    }
+    public static T MaxBy<T>(
+        this IEnumerable<T> collection,
+        Func<T, double> selector
+    )
+    {   
+        ArgumentNullException.ThrowIfNull(collection, nameof(collection));
+
+        var it = collection.GetEnumerator();
+        it.MoveNext();
+        T max = it.Current;
+        while(it.MoveNext())
+            if(selector(it.Current) > selector(max))
+                max = it.Current;
+        return max;
+    }
+    public static bool Any<T>(
+        this IEnumerable<T> collection,
+        Func<T, bool> predicate
+    )
+    {   
+        ArgumentNullException.ThrowIfNull(collection, nameof(collection));
+
+        var it = collection.GetEnumerator();
+        while(it.MoveNext())
+            if(predicate(it.Current))
+                return true;
+        return false;
+    }
+    public static R Aggregate<T, R>(
+        this IEnumerable<T> collection,
+        Func<T, R, R> acc,
+        R seed
+    )
+    {   
+        ArgumentNullException.ThrowIfNull(collection, nameof(collection));
+
+        var it = collection.GetEnumerator();
+        while(it.MoveNext())
+            seed = acc(it.Current, seed);
+        return seed;
+    }
 }
-
-
-
-
-
