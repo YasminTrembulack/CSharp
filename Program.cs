5b4282e1ec85e236ae845c171c1b5899d8a2bb80
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2024-08-13 12:57:55 -0300

    Remove arquivo grande do commit

--- Program.cs
+++ Program.cs
@@ -1,284 +1,132 @@
-﻿// PROGRAMAÇÃO FUNCIONAL 
-// public delegate void Func(string s);
-// criando um tipo chamado func 
+﻿object liste = new List<int>();
 
-Action<string> print = Console.WriteLine;
-// posso fazer minhas propias funções e colocar em um variavel ou colocar alguma existente
-print("Hello world!");
-
-
-Console.WriteLine("-----------------------");
-int minhaFuncao(int a, int b)
+if (liste is List<int>)
 {
-    return a + b;
-};
-Func<int, int, int> sum = minhaFuncao;
-var result_soma = sum(5, 2);
-print(result_soma.ToString());
+    
+}
+object obj = new List<int>();
 
-
-Console.WriteLine("-----------------------");
-Func<int, int, int> sub = (a, b) => a - b;
-var result_sub = sub(5, 2);
-print(result_sub.ToString());
-
-
-Console.WriteLine("-----------------------");
-var soma = (int[] array) =>
+if(obj.GetType() == typeof(List<int>))
 {
-    int total = 0;
-    foreach (var value in array)
-        total += value;
-    return total;     
-};
-var result_arr_soma = soma([1,2,3,4,5,6,7,8]);
-print(result_arr_soma.ToString());
+    List<int> list1 = (List<int>)obj;
+}
 
-
-Console.WriteLine("-----------------------");
-
-var chamaNVezes = (Action Func, int n) =>
+if(obj is List<int> lista)
 {
-    for (int i = 0; i < n; i++)
-        Func();
-};
-chamaNVezes( () => Console.WriteLine("oi"), 100 );
-
-
-
-Func<int, Func<int>> func = (n) =>
-{
-    return () => n + 5; 
-};
-// tem como parametro um int e me retorna uma função com retorno int
-
-
-Console.WriteLine("-----------------------");
-// da um coleção de dados e o tamanho de cada pagina
-Func<int[], int, Func<int, int[]>> paginator = (dados, tamanho) =>
-{
-    return (pagina) => {
-        int[] paginaDados = new int[tamanho];
-        Array.Copy(
-            dados, tamanho * pagina,
-            paginaDados, 0,
-            tamanho
-        );
-        return paginaDados;
-    };
-};
-var paginas = paginator([1, 2, 3, 4, 5, 6], 2);
-var pagina02 = paginas(2);
-foreach (var item in pagina02)
-    Console.WriteLine(item);
-
-
-Console.WriteLine("-----------------------");
-Func<Func<int>> closure = () =>
-{
-    int count = 0;
-    return () => {
-        count++;
-        return count;
-    };
-    // return () => ++count;
-};
-var contador = closure();
-Console.WriteLine(contador());
-Console.WriteLine(contador());
-Console.WriteLine(contador());
-Console.WriteLine(contador());
-
-//Ta errado
-// Func<Object, (Func<Object> data, Action<dynamic> setData)> useSatate = (object data) =>
-// {
-//     var value = data;
-//     return () => value, (object newValue) => value = newValue;
-
-// }
-
-Console.WriteLine("-----------------------");
-List<int> listaMinha = [1, 2, 3, 4, 5, 6, 7, 8];
-var pares = listaMinha.Where((element) => element % 2 == 0);
-foreach (var item in pares)
-    Console.WriteLine(item);
-
-
-
-Console.WriteLine("-----------------------");
-var pares_maior = listaMinha
-            .Where((element) => element % 2 == 0)
-            .Where((x) => x > 2);
-foreach (var item in pares_maior)
-    Console.WriteLine(item);
-
-
-
-Console.WriteLine("-----------------------");
-var select = listaMinha.Select((x) => x * 2);
-foreach (var item in select)
-    Console.WriteLine(item);
-
-
-
-Console.WriteLine("-----------------------");
-List<string> list_str = ["1", "2", "3", "4", "5", "6", "7", "8"];
-var list_int = list_str.Select(int.Parse);
-foreach (var item in list_int)
-    Console.WriteLine(item);
-
-
-
-Console.WriteLine("----------- Aula 04 ------------");
-var take_while = listaMinha.TakeWhile((x) => x < 5);
-foreach (var item in take_while)
-    Console.WriteLine(item);
-
-
-
-Console.WriteLine("-----------------------");
-var skip_while = listaMinha.SkipWhile((x) => x < 5);
-foreach (var item in skip_while)
-    Console.WriteLine(item);
-
-
-
-Console.WriteLine("-----------------------");
-Pessoa yasmin = new("yasmin", 18);
-Pessoa eduardo = new("eduardo", 19);
-Pessoa julia = new("julia", 16);
-
-Pessoa[] pessoas = [yasmin, eduardo, julia];
-
-var maior_idade = pessoas.MaxBy((p) => p.Idade);
-Console.WriteLine(maior_idade);
-
-
-
-Console.WriteLine("-----------------------");
-var any_false = listaMinha.Any((x) => x % 13 == 0);
-Console.WriteLine(any_false);
-var any_true = listaMinha.Any((x) => x % 7 == 0);
-Console.WriteLine(any_true);
-
-
-
-Console.WriteLine("-----------------------");
-var aggregate = listaMinha.Aggregate((x, y) => x + y, 10);
-Console.WriteLine(aggregate);
-
-public class Pessoa{
-
-    public string Nome { get; set; }
-    public int Idade { get; set; }
-
-    public Pessoa(string nome, int idade)
+    foreach (var item in lista)
     {
-        Nome = nome;
-        Idade = idade;
-    }
-    public override string ToString()
-    {
-        return "Nome: " + Nome + " Idade: " + Idade;
+        
     }
 }
 
+List<int> list = [];
 
+for (int i = 0; i < 10; i++)
+    list.Add(i);
 
-public static class Enumerable
+if (list is [1, 2, 3, .., _, int value])
 {
-    public static IEnumerable<T> Where<T>(
-        this IEnumerable<T> collection,
-        Func<T, bool> filter
-    )
+
+}
+
+
+if (list is [>10 and <20, 2 or 3, .., _])
+{
+
+}
+
+// public static implicit operator Complex((double r, double i) value)
+Complex a = (1, 2);
+
+var c1 = new Complex();
+var c2 = new Complex();
+var sum = c1 + c2;
+
+
+
+TemperaturaClassification Classify(int temperatura)
+    => temperatura switch
     {
-        foreach (var item in collection)
-            if (filter(item))
-                yield return item;
+        >30 => TemperaturaClassification.Hot,
+        <15 => TemperaturaClassification.Cold,
+        _ => TemperaturaClassification.Ok
+    };
 
-    }
-    public static IEnumerable<R> Select<T, R>(
-        this IEnumerable<T> collection,
-        Func<T, R> mapper
-    )
-    {   
-        ArgumentNullException.ThrowIfNull(collection, nameof(collection));
-        ArgumentNullException.ThrowIfNull(mapper, nameof(mapper));
+GradeClassification ClassifyGrades(int math, int cience)
+    => (math, cience) switch
+    {
+        (>90, _) or (_, >90) => GradeClassification.Good,
+        (>70, >70) => GradeClassification.Good,
+        (<50, _) and (_, <50) => GradeClassification.Bad,
+        _ => GradeClassification.Meh
+    };
 
-        var it = collection.GetEnumerator();
-        while(it.MoveNext())
-            yield return mapper(it.Current);
-    }
-    public static IEnumerable<T> TakeWhile<T>(
-        this IEnumerable<T> collection,
-        Func<T, bool> predicate
-    )
-    {   
-        ArgumentNullException.ThrowIfNull(collection, nameof(collection));
-        
-        foreach (var item in collection)
+int Count(IEnumerable<int> collection)
+    => collection switch
+    {
+        ICollection<int> coll => coll.Count,
+        null => 0,
+        _ => collection.Count()
+    };
+
+bool CanDrive(Person person)
+    => person switch
+    {
+        { IsFromDitactorFamily: true} => false,
+
+        _ => false
+    };
+
+
+public enum GradeClassification
+{
+    Good,
+    Bad,
+    Meh
+}
+
+public enum TemperaturaClassification
+{
+    Hot,
+    Ok,
+    Cold
+}
+
+public class Person
+{
+    public string Name { get; set;}
+    public int Age { get; set;}
+    public bool IsFromDitactorFamily { get; set;}
+
+}
+
+public class Complex
+{
+    public double Real { get; set; }
+
+    public double Imaginary { get; set; }
+
+    public static implicit operator Complex((double r, double i) value)
+    {
+        return new Complex
         {
-            if(!predicate(item))
-                break;
-            yield return item;
-        }
+            Real = value.r,
+            Imaginary = value.i
+        };
     }
-    public static IEnumerable<T> SkipWhile<T>(
-        this IEnumerable<T> collection,
-        Func<T, bool> predicate
-    )
-    {   
-        ArgumentNullException.ThrowIfNull(collection, nameof(collection));
-        
-        var canReturn = false;
-        foreach (var item in collection){
-            if(!predicate(item))
-                canReturn = true;
-            if(canReturn)
-                yield return item;
-        }
-    }
-    public static T MaxBy<T>(
-        this IEnumerable<T> collection,
-        Func<T, double> selector
-    )
-    {   
-        ArgumentNullException.ThrowIfNull(collection, nameof(collection));
 
-        var it = collection.GetEnumerator();
-        it.MoveNext();
-        T max = it.Current;
-        while(it.MoveNext())
-            if(selector(it.Current) > selector(max))
-                max = it.Current;
-        return max;
-    }
-    public static bool Any<T>(
-        this IEnumerable<T> collection,
-        Func<T, bool> predicate
-    )
-    {   
-        ArgumentNullException.ThrowIfNull(collection, nameof(collection));
+    public static bool operator ==(Complex c1, Complex c2)
+        => c1.Real == c2.Real && c1.Imaginary == c2.Imaginary;
 
-        var it = collection.GetEnumerator();
-        while(it.MoveNext())
-            if(predicate(it.Current))
-                return true;
-        return false;
-    }
-    public static R Aggregate<T, R>(
-        this IEnumerable<T> collection,
-        Func<T, R, R> acc,
-        R seed
-    )
-    {   
-        ArgumentNullException.ThrowIfNull(collection, nameof(collection));
+    public static bool operator !=(Complex c1, Complex c2)
+        => !(c1 == c2);
 
-        var it = collection.GetEnumerator();
-        while(it.MoveNext())
-            seed = acc(it.Current, seed);
-        return seed;
+    public static Complex operator +(Complex c1, Complex c2)
+    {
+        return new Complex
+        {
+            Real = c1.Real + c2.Real,
+            Imaginary = c1.Imaginary + c2.Imaginary
+        };
     }
 }
-
-
